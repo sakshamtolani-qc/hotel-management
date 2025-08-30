c0cb7f67d2649cf3a1eb4731d8e13949a1198164
GIT demo <anikett23.official@gmail.com>
2025-08-31 02:56:16 +0530

    [HMCRM-TTQH37 Added models.py for billings

--- billing/admin.py
+++ billing/admin.py
@@ -1,1 +1,17 @@
 # Register your models here.
+from django.contrib import admin
+from billing.models import Billing
+
+
+@admin.register(Billing)
+class BillingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        # "reservation",
+        "user",
+        "total_amount",
+        "payment_status",
+        "created_at",
+    )
+    list_filter = ("payment_status", "payment_method", "created_at")
+    search_fields = ("transaction_id", "reservation__id", "user__username")
