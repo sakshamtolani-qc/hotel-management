39c1075b48377e84aebf28ced03f0f11b51943d9
sakshamtolani-qc <hr@quoriumconsulting.com>
2025-08-31 23:31:16 +0530

    Add supabase (#10)
    * Merge branch 'main' of https://github.com/sakshamtolani-qc/hotel-management
    
    * Added Secrets Manager along with Supabase Postgres DB

--- src/billing/admin.py
+++ src/billing/admin.py
@@ -1,4 +1,5 @@
 # Register your models here.
+<<<<<<< HEAD
 from django.contrib import admin
 from billing.models import Billing
 
@@ -16,3 +17,5 @@ class BillingAdmin(admin.ModelAdmin):
     list_filter = ("payment_status", "payment_method", "created_at")
     search_fields = ("transaction_id", "reservation__id", "user__username")
 
+=======
+>>>>>>> 5ba5004 (Add supabase (#10))
